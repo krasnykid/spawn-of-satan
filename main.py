0803ee755e80868001a083b79c01771c8aa4dca8
krasnykid <krasnykid@github.com>
2025-05-30 12:40:45 +0200

    applying gabor filter to image

---
+++ main.py
@@ -0,0 +1,57 @@
+import cv2
+import numpy as np
+import matplotlib.pyplot as plt
+from PIL import Image
+
+def create_gaborfilter():
+    # This function is designed to produce a set of GaborFilters
+    # an even distribution of theta values equally distributed amongst pi rad / 180 degree
+
+    filters = []
+    num_filters = 16
+    ksize = 35  # The local area to evaluate
+    sigma = 3.0  # Larger Values produce more edges
+    lambd = 10.0
+    gamma = 0.5
+    psi = 0  # Offset value - lower generates cleaner results
+    for theta in np.arange(0, np.pi, np.pi / num_filters):  # Theta is the orientation for edge detection
+        kern = cv2.getGaborKernel((ksize, ksize), sigma, theta, lambd, gamma, psi, ktype=cv2.CV_64F)
+        kern /= 1.0 * kern.sum()  # Brightness normalization
+        filters.append(kern)
+    return filters
+
+def apply_filter(img, filters):
+# This general function is designed to apply filters to our image
+
+    # First create a numpy array the same size as our input image
+    newimage = np.zeros_like(img)
+
+    # Starting with a blank image, we loop through the images and apply our Gabor Filter
+    # On each iteration, we take the highest value (super impose), until we have the max value across all filters
+    # The final image is returned
+    depth = -1 # remain depth same as original image
+
+    for kern in filters:  # Loop through the kernels in our GaborFilter
+        # image_filter = cv2.filter2D(img, depth, kern)  #Apply filter to image
+        image_filter = cv2.filter2D(img, depth, kern)
+
+        # Using Numpy.maximum to compare our filter and cumulative image, taking the higher value (max)
+        np.maximum(newimage, image_filter, newimage)
+    return newimage
+
+original_image = Image.open('photos/git1.jpg')
+original_array = np.array(original_image)
+
+'''create gabor filter'''
+gfilters = create_gaborfilter()
+
+'''get filtered image array'''
+gf_array = apply_filter(original_array, gfilters)
+
+'''make pil image from array'''
+gf_image = Image.fromarray(gf_array)
+
+'''display image'''
+plt.imshow(np.array(gf_image))
+plt.tight_layout
+plt.show()
